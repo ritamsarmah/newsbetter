943823190041208dae321e0b1aac76e652edfa42
Ritam Sarmah <sarmah.ritam@gmail.com>
2021-01-04 10:30:05 -0800

    Clean up code

--- newsbetter.py
+++ newsbetter.py
@@ -6,42 +6,44 @@ import ssl
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from jinja2 import Template, Environment, BaseLoader, DebugUndefined
+from jinja2 import Template
 
 
 class Newsletter:
-    default_smtp_port = 25
-
     def __init__(self, html_template, sections=[]):
         self.html_template = Template(html_template)
-        self.sections = set(sections)
+        self.sections = {
+            section.identifier: section for section in sections}
 
     def add_section(self, section):
-        self.sections.add(section)
+        self.sections[section.identifier] = section
 
     def render(self, recipient):
-        identifiers = {section.identifier: section.html(recipient)
-                       for section in self.sections}
+        identifiers = {identifier: section.html(recipient)
+                       for (identifier, section) in self.sections.items()}
 
         rendered_email = self.html_template.render(**identifiers)
         return rendered_email
 
     def send(self, sender, subject, recipients):
+        DEFAULT_SMTP_PORT = 25
+
         # Create secure connection with server and send email to recipients
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(sender["smtp"], sender.get("port", Newsletter.default_smtp_port), context=context) as server:
+        with smtplib.SMTP_SSL(sender["smtp"], sender.get("port", DEFAULT_SMTP_PORT), context=context) as server:
             server.login(sender["email"], sender["password"])
 
             for recipient in recipients:
-                # Create message container - the correct MIME type is multipart/alternative.
                 message = MIMEMultipart('alternative')
                 message['Subject'] = subject
                 message['From'] = f"{sender['name']} <{sender['email']}>"
                 message['To'] = recipient["email"]
                 message.attach(MIMEText(self.render(recipient), "html"))
 
-                server.sendmail(
-                    sender["email"], recipient['email'], message.as_string())
+                server.sendmail(sender['email'],
+                                recipient['email'],
+                                message.as_string())
+
                 print(f"Sent: {recipient['name']} <{recipient['email']}>")
 
 
